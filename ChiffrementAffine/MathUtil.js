89e9b7380c1c89f7711124ecfabf849285bb4177
loicArif <loic.arif@sstrn.fr>
2018-10-30 14:39:32 +0100

    Refacto MathUtil.findKey

--- ChiffrementAffine/MathUtil.js
+++ ChiffrementAffine/MathUtil.js
@@ -21,71 +21,98 @@ class MathUtil {
 
     static findKey(pente, modulo, ORIGINAL_PENTE, equationArray) {
         equationArray = equationArray || [];
-        if(pente === 1 && equationArray.length === 0) {
-            return pente;
-        } else {
-            let resultat = Math.trunc(modulo / pente);
-            let reste = MathUtil.modulo(modulo, pente);
-            let inverseReste = resultat * -1;
-            equationArray.unshift({
-                reste: reste,
-                pente: pente,
-                modulo: modulo,
-                inverseReste: inverseReste,
-                stringEq: `${reste} = ${modulo} + ${pente} * ${inverseReste}`
-            });
-            if (reste === 1) {
-                let quotientHolder = [];
-                let historiques = [];
-                for(let i = 0;i < equationArray.length;i++) {
-                    historiques.push({
-                        reste: equationArray[i].reste,
-                        quotients: []
-                    });
-                    historiques[i].quotients.push(equationArray[i].inverseReste);
-                    if(equationArray[i].modulo === ORIGINAL_PENTE) {
-                        historiques[i].quotients.push(1);
-                    }
-                }
-                let originalHistoriques = historiques;
-                for(let i = equationArray.length-1;i >= 0;i--) {
-                    let currentMultiplicater = equationArray[i].inverseReste;
-                    historiques.forEach(historique => {
-                        if(quotientHolder.length > 0 && (equationArray[i].pente === historique.reste)) {
-                            historique.quotients = quotientHolder;
-                            quotientHolder = [];
-                        }
-                        if(equationArray[i].modulo === historique.reste) {
-                            originalHistoriques.forEach(originalHistorique => {
-                                if(originalHistorique.reste === historique.reste) {
-                                    originalHistorique.quotients.forEach(quotient => {
-                                        quotientHolder.push(quotient);
-                                    })
-                                }
-                            })
-                        }
-                        if(equationArray[i].pente === historique.reste) {
-                            let somme = 0;
-                            for(let j = 0;j < historique.quotients.length;j++) {
-                                somme = somme + historique.quotients[j] * currentMultiplicater;
-                            }
-                            if(somme !== 0) {
-                                quotientHolder.push(somme)
-                            }
-                        }
-                    });
-                    if(equationArray[i].modulo === ORIGINAL_PENTE) {
-                        quotientHolder.push(1);
-                    }
-                }
-                let finalResult = 0;
-                quotientHolder.forEach(quotient => {
-                    finalResult = finalResult + quotient;
-                });
-                return finalResult;
-            }
-            return MathUtil.findKey(reste, pente, ORIGINAL_PENTE, equationArray);
+        if(pente === 1 && equationArray.length === 0) return pente;
+        let resultat = Math.trunc(modulo / pente);
+        let reste = MathUtil.modulo(modulo, pente);
+        let inverseReste = resultat * -1;
+        MathUtil._fillEquationArray(equationArray, reste, pente, modulo, inverseReste);
+        if (reste === 1) {
+            return MathUtil._outExecution(equationArray, ORIGINAL_PENTE);
         }
+        return MathUtil.findKey(reste, pente, ORIGINAL_PENTE, equationArray);
+    }
+
+    static _fillEquationArray(equationArray, reste, pente, modulo, inverseReste) {
+        equationArray.unshift({
+            reste: reste,
+            pente: pente,
+            modulo: modulo,
+            inverseReste: inverseReste,
+            stringEq: `${reste} = ${modulo} + ${pente} * ${inverseReste}`
+        });
+    }
+
+    static _outExecution(equationArray, ORIGINAL_PENTE) {
+        let equationDatas = MathUtil._scanEquations(equationArray, ORIGINAL_PENTE);
+        const originalEquationDatas = equationDatas;
+        let quotientHolder = [];
+        for (let i = equationArray.length - 1; i >= 0; i--) {
+            let currentMultiplicater = equationArray[i].inverseReste;
+            equationDatas.forEach(equationData => {
+                quotientHolder = MathUtil._getQuotientsFromUpwardEquation(quotientHolder, equationArray, i, equationData);
+                MathUtil._getQuotientsFromUpwardEquationWithModulo(equationArray, i, equationData, originalEquationDatas, quotientHolder);
+                MathUtil._getQuotientsFromUpwardEquationWithSlope(equationArray, i, equationData, currentMultiplicater, quotientHolder);
+            });
+            if (equationArray[i].modulo === ORIGINAL_PENTE) {
+                quotientHolder.push(1);
+            }
+        }
+        return MathUtil._calculateResult(quotientHolder);
+    }
+
+    static _scanEquations(equationArray, ORIGINAL_PENTE) {
+        let equationDatas = [];
+        for (let i = 0; i < equationArray.length; i++) {
+            equationDatas.push({
+                reste: equationArray[i].reste,
+                quotients: []
+            });
+            equationDatas[i].quotients.push(equationArray[i].inverseReste);
+            if (equationArray[i].modulo === ORIGINAL_PENTE) {
+                equationDatas[i].quotients.push(1);
+            }
+        }
+        return equationDatas;
+    }
+
+    static _getQuotientsFromUpwardEquation(quotientHolder, equationArray, i, equationData) {
+        if (quotientHolder.length > 0 && (equationArray[i].pente === equationData.reste)) {
+            equationData.quotients = quotientHolder;
+            quotientHolder = [];
+        }
+        return quotientHolder;
+    }
+
+    static _getQuotientsFromUpwardEquationWithModulo(equationArray, i, equationData, originalEquationDatas, quotientHolder) {
+        if (equationArray[i].modulo === equationData.reste) {
+            originalEquationDatas.forEach(originalEquationData => {
+                if (originalEquationData.reste === equationData.reste) {
+                    originalEquationData.quotients.forEach(quotient => {
+                        quotientHolder.push(quotient);
+                    })
+                }
+            })
+        }
+    }
+
+    static _getQuotientsFromUpwardEquationWithSlope(equationArray, i, equationData, currentMultiplicater, quotientHolder) {
+        if (equationArray[i].pente === equationData.reste) {
+            let somme = 0;
+            for (let j = 0; j < equationData.quotients.length; j++) {
+                somme = somme + equationData.quotients[j] * currentMultiplicater;
+            }
+            if (somme !== 0) {
+                quotientHolder.push(somme)
+            }
+        }
+    }
+
+    static _calculateResult(quotientHolder) {
+        let finalResult = 0;
+        quotientHolder.forEach(quotient => {
+            finalResult = finalResult + quotient;
+        });
+        return finalResult;
     }
 
     static constructInverseEq(inversedKey, affFunction, modulo) {
