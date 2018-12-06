b098db3a39f48391156f07255c88065ac904585e
snipzer <l.arif@laposte.net>
2018-12-06 16:12:14 +0100

    programme2

---
+++ MathPython/programme2.py
@@ -0,0 +1,71 @@
+from sympy import *
+
+
+def execute_explicit(suite, rank):
+    x = var('x')
+    sympy_suite = sympify(suite)
+    return sympy_suite.subs(x, rank)
+
+
+def execute_recurent(suite, initialTerm, rank):
+    x = var('x')
+    sympy_suite = sympify(suite)
+    i = 0
+    while i < rank:
+        initialTerm = sympy_suite.subs(x, initialTerm)
+        i = i + 1
+    return initialTerm
+
+
+def is_arithmetique(suite, initialRank=False):
+    value_holder = []
+    i = 1
+    while i <= 6:
+        if not initialRank:
+            value_holder.append(int(execute_explicit(suite, i)))
+        else:
+            value_holder.append(int(execute_recurent(suite, initialRank, i)))
+        i = i + 1
+    first_test = abs(value_holder[0] - value_holder[1])
+    second_test = abs(value_holder[2] - value_holder[3])
+    third_test = abs(value_holder[4] - value_holder[5])
+    return first_test == second_test == third_test
+
+
+def is_geometrique(suite, initialRank=False):
+    value_holder = []
+    i = 1
+    while i <= 6:
+        if not initialRank:
+            value_holder.append(int(execute_explicit(suite, i)))
+        else:
+            value_holder.append(int(execute_recurent(suite, initialRank, i)))
+        i = i + 1
+    first_test = value_holder[1] / value_holder[0]
+    second_test = value_holder[3] / value_holder[2]
+    third_test = value_holder[5] / value_holder[4]
+    return first_test == second_test == third_test
+
+
+firstChoice = int(input("Type 1 for an explicite or 2 for recurrent... => "))
+if firstChoice == 1:
+    suite = input("Enter the expression of the function with 'x' as variable... => ")
+    if is_arithmetique(suite):
+        print("Cette suite est arithmétique.")
+    elif is_geometrique(suite):
+        print("Cette suite est géométrique.")
+    else:
+        print("Cette suite n'est ni arithmétique ni géométrique.")
+elif firstChoice == 2:
+    suite = input("Enter the expression of the function with 'x' as variable... => ")
+    initialTerm = int(input("Enter the value for U0... => "))
+    rank = int(input("Enter the rank for x... => "))
+    if is_arithmetique(suite, rank):
+        print("Cette suite est arithmétique.")
+    elif is_geometrique(suite, rank):
+        print("Cette suite est géométrique.")
+    else:
+        print("Cette suite n'est ni arithmétique ni géométrique.")
+else:
+    print('Invalid input')
+
