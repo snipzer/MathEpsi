75034a102ad6f01ab4a40987a90dfd00a3443e5a
snipzer <l.arif@laposte.net>
2018-12-16 19:23:46 +0100

    Fix programme 5

--- MathPython/programme5.py
+++ MathPython/programme5.py
@@ -3,14 +3,17 @@ from sympy.abc import x
 
 
 def is_limit(suite):
-    res = limit(suite, x, oo)
-    if res == oo:
+    res = limit_seq(suite, x)
+    if res == oo or res == -oo:
         return "Suite divergente"
-    elif res == -oo:
+    elif res is None:
+        return "Pas de limite"
+    elif res.is_real:
         return "Suite convergente"
     else:
-        return "Pas de limite"
+        return "Il semble y avoir un cas particulier non prévue..."
 
 
-suite = sympify(input("Entrée la fonction pour déterminer sa limite :\n"))
+suite = sympify(input("Entrée la fonction pour déterminer sa limite(avec x en tant que variable) : "))
 print(is_limit(suite))
+
