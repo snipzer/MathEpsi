19fc03e01e8533e5be4223018940abc36ba417f5
snipzer <l.arif@laposte.net>
2018-12-21 16:33:02 +0100

    HubSimplification

--- MathJs/src/index.js
+++ MathJs/src/index.js
@@ -1,5 +1,8 @@
-const HubService = require('./services/HubService');
+const PackageJson = require('../package');
 
-const hubService = new HubService();
-hubService.listProcess();
-
+const algorithm = PackageJson.scripts;
+console.log('Here the list of all available process');
+delete algorithm.start;
+Object.keys(algorithm).forEach(function(key, keyIndex) {
+    console.log(`${keyIndex+1}: ${key}, do => npm run ${key}`);
+});
