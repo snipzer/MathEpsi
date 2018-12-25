35f505c212068d503317683e0e783afc5c4675ff
snipzer <l.arif@laposte.net>
2018-12-25 15:44:06 +0100

    Adding chineseRemainderExec

--- MathJs/src/executions/chineseRemainderExec.js
+++ MathJs/src/executions/chineseRemainderExec.js
@@ -1,5 +1,6 @@
 const ChineseRemainder = require('../process/ChineseRemainder');
 const CREquationHolder = require('../process/ChineseRemainderEquationHolder');
+const Readline = require('readline');
 
 // Pos test
 // let eq1 = "0 %25";
@@ -11,9 +12,47 @@ const CREquationHolder = require('../process/ChineseRemainderEquationHolder');
 // let eq3 = "4 % 11";
 // result = 1731;
 
-const chineseRemainder = new ChineseRemainder([
-    new CREquationHolder(0, 25),
-    new CREquationHolder(-3, 31),
-]);
+const rl = Readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-console.log(chineseRemainder.execute());
+const askEquationNumber = () => {
+    return new Promise(resolve => {
+        rl.question('How many equation ? ', (question) => {
+            resolve(question);
+        })
+    })
+};
+
+const getSlope = () => {
+    return new Promise(resolve => {
+        rl.question('Choose the slope : ', (slope) => {
+            resolve(slope)
+        })
+    })
+};
+
+const getModulo = () => {
+    return new Promise(resolve => {
+        rl.question('Choose the modulo : ', (modulo) => {
+            resolve(modulo)
+        })
+    })
+};
+
+const main = async () => {
+    let number = await askEquationNumber();
+    let equationArray = [];
+    for(let i = 1; i <= number; i++) {
+        console.log(`For the equation n°: ${i}...`);
+        let slope = await getSlope();
+        let modulo = await getModulo();
+        equationArray.push(new CREquationHolder(slope, modulo));
+    }
+    const chineseRemainder = new ChineseRemainder(equationArray);
+    console.log(chineseRemainder.execute());
+    rl.close()
+};
+
+main();
